50c477e4b983aef3aaaec9547b3227cd01355455
kg-phantom <ksurbayan@gmail.com>
2022-01-14 12:06:34 -0800

    success page and order history

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -12,7 +12,7 @@ const StoreProvider = ({ value = [], ...props }) => {
         categories: [],
         currentCategory: '',
     });
-    console.log(state);
+    
     return <Provider value={[state, dispatch]} {...props} />;
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
     product(_id: ID!): Product
     user: User
     order(_id: ID!): Order
-    checkout(products: [ID!]): Checkout
+    checkout(products: [ID]!): Checkout
   }
 
   type Mutation {
