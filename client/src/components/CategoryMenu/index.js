352190892f28de21bac29caf7c3be1f9b5e7b03b
kg-phantom <ksurbayan@gmail.com>
2022-01-13 23:12:47 -0800

    cart items persist:

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { idbPromise } from '../../utils/helpers';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY, UPDATE_PRODUCTS } from '../../utils/actions';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from '../../utils/GlobalState';
